a5547f913d2b06e00b6e0b930cfb73e6a436eaea
Robin Monnier <monnier.robin@gmail.com>
2018-05-22 18:24:05 +0200

    add start training

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,14 +4,50 @@ const router = require('express').Router()
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 
 // Handles messages events
+function sendMessage (sender_psid, toSend) {
+  let response
+
+  // Create the payload for a basic text message
+  response = {
+    'text': toSend
+  }
+
+  // Sends the response message
+  callSendAPI(sender_psid, response)
+}
+
+// Handles messages events
 function handleMessage (sender_psid, received_message) {
   let response
 
   // Check if the message contains text
   if (received_message.text) {
+    sendMessage(sender_psid, 'Salut ! Je suis Joe, ton coach sportif 💪')
+    sendMessage(sender_psid, 'J\'éspère que tu as le coeur accroché parce que tes muscles vont chauffer.')
     // Create the payload for a basic text message
     response = {
-      'text': `You sent the message: "${received_message.text}". Now send me an image!`
+      'attachment': {
+        'type': 'template',
+        'payload': {
+          'template_type': 'button',
+          'text': 'Pour quoi tu veux t\'entrainer ?',
+          'buttons': [{
+            'type': 'postback',
+            'title': 'Je veux juste garder la forme!',
+            'payload': 'START_REGULAR'
+          },
+          {
+            'type': 'postback',
+            'title': 'Half Iron Man',
+            'payload': 'START_HALF_IRON_MAN'
+          },
+          {
+            'type': 'postback',
+            'title': 'Iron Man',
+            'payload': 'START_IRON_MAN'
+          }]
+        }
+      }
     }
   }
 
@@ -21,7 +57,21 @@ function handleMessage (sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback (sender_psid, received_postback) {
+  let response
 
+  // Get the payload for the postback
+  let payload = received_postback.payload
+
+  // Set the response based on the postback payload
+  if (payload === 'START_REGULAR') {
+    response = { 'text': 'Ah donc tu n\'est pas très ambitieux. Je reviens vers toi dès que ton programme d\'entraînement est prêt!' }
+  } else if (payload === 'START_HALF_IRON_MAN') {
+    response = { 'text': 'A moitié courageux, c\'est toujours ça.' }
+  } else if (payload === 'START_IRON_MAN') {
+    response = { 'text': 'Excellent choix! Les prochaines semaines vont être difficile parce que... ça va chier.' }
+  }
+  // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response)
 }
 
 // Sends response messages via the Send API
@@ -33,7 +83,7 @@ function callSendAPI (sender_psid, response) {
     },
     'message': response
   }
-	console.log(request_body);
+  console.log(request_body)
 
   // Send the HTTP request to the Messenger Platform
   request({
@@ -47,7 +97,7 @@ function callSendAPI (sender_psid, response) {
     } else {
       console.error('Unable to send message:' + err)
     }
-  }) 
+  })
 }
 
 // Accepts POST requests at /webhook endpoint
@@ -71,7 +121,7 @@ router.post('/', (req, res) => {
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message)        
+        handleMessage(sender_psid, webhook_event.message)
       } else if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback)
       }
@@ -79,7 +129,6 @@ router.post('/', (req, res) => {
 
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED')
-
   } else {
     // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404)
@@ -89,13 +138,13 @@ router.post('/', (req, res) => {
 // Accepts GET requests at the /webhook endpoint
 router.get('/', (req, res) => {
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = 'Toto75017';
+  const VERIFY_TOKEN = 'Toto75017'
 
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode']
   let token = req.query['hub.verify_token']
   let challenge = req.query['hub.challenge']
-    
+
   // Check if a token and mode were sent
   if (mode && token) {
     // Check the mode and token sent are correct
@@ -103,10 +152,9 @@ router.get('/', (req, res) => {
       // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED')
       res.status(200).send(challenge)
-    
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403)      
+      res.sendStatus(403)
     }
   }
 })
